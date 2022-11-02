b37f39086677616ffc687f8de408fe1555a647d4
t0uu <srtouzet@hotmail.com>
2022-11-02 10:47:46 -0300

    add chapter

---
+++ src/Experience.js
@@ -0,0 +1,18 @@
+import { OrbitControls } from '@react-three/drei'
+import { Physics,Debug } from '@react-three/rapier'
+import { Level } from './Level.js'
+import Lights from './Lights.js'
+
+export default function Experience()
+{
+    return <>
+
+        <OrbitControls makeDefault />
+        <Physics>
+            <Debug/>
+        <Lights />
+        <Level/>
+        </Physics>
+
+    </>
+}
